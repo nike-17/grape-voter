31b1031f57c5b9f5f13e85818c84587b2048e7de
nike-17 <nike-17@ya.ru>
2015-11-12 06:17:17 -0600

    update capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,7 @@ set :repo_url, 'git@github.com:nike-17/grape-voter.git'
 
 server 'putin.io', user: 'root', roles: %w{web app}
 
-set :linked_dirs, %w{bin log shared vendor/bundle}
-
+set :linked_dirs, %w{bin vendor/bundle shared/log shared/pids shared/sockets }
 
 # how many old releases do we want to keep, not much
 set :keep_releases, 5
