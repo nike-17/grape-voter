8920f841d7aae97517405fc2b93f258bfbcae319
nike-17 <nike-17@ya.ru>
2016-01-03 12:23:59 -0600

    add top

--- lib/tasks/data.rb
+++ lib/tasks/data.rb
@@ -6,8 +6,16 @@ namespace :data do
   end
   desc 'Generate top file'
   task :generate_top => :environment do
-  	File.open("/www/putin.io/data/top.json","w") do |f|
-  		f.write(JSON.generate(Models::Vote.top))
+  	  	# File.open("/www/putin.io/data/top.json","w") do |f|
+
+  	File.open("top.json","w") do |f|
+  		top = Models::Vote.top
+  		top.each do |key, array|
+  			top[key]  = array.map { |e|  
+  				{:name => e.name.force_encoding("UTF-8"), :count => e.ammount_sum}
+  			}
+		end
+  		f.write(top.to_json)
 	end
   end 
 end
