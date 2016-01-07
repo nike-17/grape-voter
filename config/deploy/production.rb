f50a7316b72688761b9971bda97b536c7a0b5d32
nike-17 <nike-17@ya.ru>
2016-01-06 22:13:18 -0600

    hide secrets

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,6 +9,8 @@ server 'putin.io', user: 'root', roles: %w{web app}
 
 set :linked_dirs, %w{bin vendor/bundle shared/log shared/pids shared/sockets }
 set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/mail.rb}
+
 
 # how many old releases do we want to keep, not much
 set :keep_releases, 5
