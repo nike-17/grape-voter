9a6191e718b6f48f063f4759a4f538ff6d198b87
nike-17 <nike-17@ya.ru>
2016-01-08 08:05:31 -0600

    attach vites to candiates

---
+++ db/migrate/20160108135457_votes_attache_candidates_again.rb
@@ -0,0 +1,17 @@
+class VotesAttacheCandidatesAgain < ActiveRecord::Migration
+  def change
+  	candidates = {}
+  	Models::Vote.where("subject = 'pro' or subject = 'contra' and candidate_id is null").each do |vote|
+  		name = vote.name
+  		name.gsub! ',', '_'
+  		name.gsub! ' ', '_'
+
+  		puts name;
+  		unless candidates[name].present?
+  			candidates[name] = Models::Candidate.where("name LIKE :name", name: name).first
+		end
+		vote.candidate = candidates[name]
+		vote.save
+  	end
+  end
+end
