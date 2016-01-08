69d64ab88adebdf9d093f6e81142d784a365e3f7
nike-17 <nike-17@ya.ru>
2016-01-08 08:04:22 -0600

    attach vites to candiates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108133152) do
+ActiveRecord::Schema.define(version: 20160108135457) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name",        limit: 255,   null: false
